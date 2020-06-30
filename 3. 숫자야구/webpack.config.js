dee63f3768d539f7d5797d0080a361f58099ccd2
sujl95 <sujl95@naver.com>
2020-07-01 01:12:50 +0900

    NumberBaseball commit

--- 3. 숫자야구/webpack.config.js
+++ 3. 숫자야구/webpack.config.js
@@ -1,15 +1,25 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin'); //node require로 부른다
+const path = require('path');
 module.exports = {
+    mode : 'development',
+    devtool : 'eval',
+    resolve : {
+        extensions : ['.js','.vue'], //확장자 필요없어짐
+    },
     entry : { //핵심파일을 넣어준다
-        app: './main.js', // app : 나중에 합쳐질 파일의 이름
+        app: path.join(__dirname,'main.js'), // app : 나중에 합쳐질 파일의 이름
     },
     module : { //webpack의 핵심 모듈 
-        rules :[{
-            
+        rules :[{ 
+            test : /\.vue$/, //정규식 .vue로 끝나는 파일인것들 
+            use : 'vue-loader', //template 처리 use = loader같음
         }],
     },
-    plugins : [],
+    plugins : [ //module들이 처리하다 후처리 
+        new VueLoaderPlugin(),
+    ],
     output : {
         filename : '[name].js', //최종 결과물
-        path : './dist', //폴더경로
+        path : path.join(__dirname,'dist'), //폴더경로
     },
 };
