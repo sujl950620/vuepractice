3e6764b80a6e5c3c22f7bbb8cf47096844eb1d4b
sujl950620 <tlaf2000@naver.com>
2020-07-02 01:55:01 +0900

    반응속도체크

---
+++ 4. 반응속도체크/main.js
@@ -0,0 +1,4 @@
+import Vue from 'vue' ; //vue를 사용하겠다는 명시 
+import ResponseCheck from './ResponseCheck';
+
+new Vue(ResponseCheck).$mount('#root');
---
+++ 4. 반응속도체크/webpack.config.js
@@ -0,0 +1,32 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin'); //node require로 부른다
+const path = require('path');
+module.exports = {
+    mode : 'development',
+    devtool : 'eval',
+    resolve : {
+        extensions : ['.js','.vue'], //확장자 필요없어짐
+    },
+    entry : { //핵심파일을 넣어준다
+        app: path.join(__dirname,'main.js'), // app : 나중에 합쳐질 파일의 이름
+    },
+    module : { //webpack의 핵심 모듈 
+        rules :[{ 
+            test : /\.vue$/, //정규식 .vue로 끝나는 파일인것들 
+            use : 'vue-loader', //template 처리 use = loader같음
+        }, {
+            test : /\.css$/,
+            use : [ 
+                'vue-style-loader',
+                'css-loader',
+            ]
+        }],
+    },
+    plugins : [ //module들이 처리하다 후처리 
+        new VueLoaderPlugin(),
+    ],
+    output : {
+        filename : '[name].js', //최종 결과물
+        path : path.join(__dirname,'dist'), //폴더경로
+        publicPath : '/dist' // 바로 보여지는거 
+    },
+};
